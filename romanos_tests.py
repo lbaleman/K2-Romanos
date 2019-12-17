0527230b3428b89e12a266ec9fc1ebb518bb0b75
Luis Bastos <luisba93@gmail.com>
2019-12-17 17:59:58 +0000

    Ejercicios terminados sin clase

--- romanos_tests.py
+++ romanos_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from romanosClase import romano_a_arabigo, arabigo_a_romano
+from romanosClase import romano_a_arabigo, contarParentesis, arabigo_a_romano, dividirgt1000
 
 class RomanNumberTest(unittest.TestCase):
     def test_symbols_romas(self):
@@ -30,6 +30,35 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(romano_a_arabigo('IL'),0)
         self.assertEqual(romano_a_arabigo('CM'),900)
         self.assertEqual(romano_a_arabigo('VV'),0)
+
+    def test_procesar_parentesis(self):
+        self.assertEqual(contarParentesis('(IV)'),[[1,'IV'],[0,'']])
+        self.assertEqual(contarParentesis('((VII))(XL)CCCXXII'),[[2,'VII'],[1,'XL'],[0,'CCCXXII']])
+        self.assertEqual(contarParentesis('(VI)((VII))'),0)
+        self.assertEqual(contarParentesis('(VI)((VII)'),0)
+        #self.assertEqual(contarParentesis('VI)((VII'),0)
+
+class ArabicNumberTest(unittest.TestCase):
+    def test_unidades(self):
+        self.assertEqual(arabigo_a_romano(1), 'I')
+        self.assertEqual(arabigo_a_romano(2), 'II')
+        self.assertEqual(arabigo_a_romano(4), 'IV')
+
+    def test_arabic_a_roman(self):
+        self.assertEqual(arabigo_a_romano(2123), 'MMCXXIII')
+        self.assertEqual(arabigo_a_romano(2444), 'MMCDXLIV')
+        self.assertEqual(arabigo_a_romano(3555), 'MMMDLV')
+        self.assertEqual(arabigo_a_romano(1678), 'MDCLXXVIII')
+        self.assertEqual(arabigo_a_romano(2999), 'MMCMXCIX')
+
+    def test_arabic_a_roman_gt_3999(self):
+        self.assertEqual(arabigo_a_romano(4000), '(IV)')
+        self.assertEqual(arabigo_a_romano(7763147686), '(((VII)))((DCCLX))(MMMCXLVII)DCLXXXVI')
+        
+    def test_gruposde1000(self):
+        self.assertEqual(dividirgt1000(7763147686), [[3,7],[2,760],[1,3147],[0,686]])
+        self.assertEqual(dividirgt1000(3763142686), [[3,0], [2,3760],[1,3140],[0,2686]])
+
 
 if __name__=='__main__':
     unittest.main()
